093a1684b756d0ce45002e0918bd2482ee3b0ae6
Haarst, Jan van <jan.vanhaarst@wur.nl>
2016-10-17 12:48:53 +0200

    No need have a python file around, as strip_path_level is now defines elsewhere.

--- src/rules/rnaseq/Tophat.py
+++
@@ -1,9 +0,0 @@
-import os.path
-import configparser
-import io
-
-def strip_path_level(path, level = 0):
-    head = path
-    for i in range(0, level):
-        (head, tail) = os.path.split(head)
-    return head
